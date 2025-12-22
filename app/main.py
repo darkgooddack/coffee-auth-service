22f34b7d4333ec80ce1589af002f40ec8a05cf51
ulyana <ulyanaworkpage@gmail.com>
2025-12-22 16:53:18 +0300

    add /health, refactor import

--- app/main.py
+++ app/main.py
@@ -1,6 +1,4 @@
-from urllib.request import Request
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 from app.core.config import settings
 from app.utils.error import AppBaseError
@@ -22,3 +20,8 @@ async def shortener_base_error_handler(
 
 
 app.include_router(router, prefix=settings.api.prefix)
+
+
+@app.get("/health")
+async def health():
+    return {"status": "ok"}
